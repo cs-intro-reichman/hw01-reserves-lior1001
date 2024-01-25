1e419f217ffb42e613d74e2b7ad5114586520038
Lior Dadonne <Liorgal.dadonne@runi.ac.il>
2024-01-25 11:41:43 +0200

    fixed gen three

--- GenThree.java
+++ GenThree.java
@@ -1,9 +1,25 @@
-/*
- * Generates three random integers, each in a given range [a,b),
- * prints them, and then prints the minimal number that was generated.
- */
 public class GenThree {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+    public static void main(String[] args) {
+        //Recieves a, b from user
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        
+        //Generates 3 random numbers within the given range
+        int r1 = (int) (a + Math.random() * (b-a));
+        int r2 = (int) (a + Math.random() * (b-a));
+        int r3 = (int) (a + Math.random() * (b-a));
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+
+        
+        //Calculates the minimal generated number 
+        int min1 = Math.min(r1, r2);
+        int min2 = Math.min(min1, r3);
+        
+        System.out.println("The minimal generated number was " + min2);
+
+        }
+
 }
